4a3e3679e5af495e8065c2828755137956a1997e
Hepheir <hepheir@gmail.com>
2024-11-10 13:37:02 +0900

    테스트케이스 생성기 템플릿 업데이트

--- problems/.templates/testcases.py
+++ problems/.templates/testcases.py
@@ -14,13 +14,13 @@ TESTCASE_DIR = BASE_DIR/"testcase"
 TESTCASE_ZIP_FILE = BASE_DIR/"testcase.zip"
 
 
+# random 모듈의 함수들로 생성되는 값이 일관성을 갖도록 시드 설정
+random.seed(0)
+
 ###############################################################
 # 문제 생성과 관련된 상수, 유틸리티 함수 정의
 # (예: N의 최댓값, 최솟값 정의 등)
 ###############################################################
-
-# random 모듈의 함수들로 생성되는 값이 일관성을 갖도록 시드 설정
-random.seed(0)
 
 MIN_N = 1
 MAX_N = 10000
@@ -30,24 +30,30 @@ MAX_N = 10000
 # 테스트케이스 생성
 ###############################################################
 
-problem = Problem(answer_file=SOLUTION_FILE)
+def testcases_for_accuracy(problem: Problem) -> None:
+    # 정확성 테스트케이스 생성
+    with problem.testcase("1") as sys:
+        sys.stdin.write("7")
+
+    ...
 
 
-# 예제 테스트케이스 생성
-
-with problem.testcase("1") as sys:
-    sys.stdin.write("7")
-
-
-# 랜덤 테스트케이스 생성
-
-
-for t in range(2, 10+1):
-    with problem.testcase(f"{t}") as sys:
+def testcases_for_efficiency(problem: Problem) -> None:
+    # 효율성 테스트케이스 생성
+    with problem.testcase("6") as sys:
         N = random.randint(MIN_N, MAX_N)
         sys.stdin.write(f"{N}")
 
+    ...
 
+
+###############################################################
 # 생성한 테스트케이스 저장
+###############################################################
+
+problem = Problem(answer_file=SOLUTION_FILE)
+
+testcases_for_accuracy(problem)
+testcases_for_efficiency(problem)
 
 problem.save(zipname=TESTCASE_ZIP_FILE, dirname=TESTCASE_DIR)
