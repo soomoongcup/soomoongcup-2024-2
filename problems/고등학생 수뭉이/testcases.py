2cf9bc4b6d669b14a24bc47504991993d7a01176
jamey <kimjunhwan@gimjunhwan-ui-MacBookPro.local>
2024-11-10 13:42:25 +0900

    refactor: 고등학생 수뭉이 README 수정 및 testcase 수정

--- problems/고등학생 수뭉이/testcases.py
+++ problems/고등학생 수뭉이/testcases.py
@@ -4,7 +4,7 @@ import string
 
 from oj import Problem
 
-MAX_N = 10000
+MAX_N = 100
 TEST_COUNT = 10
 
 DIR = pathlib.Path("/Users/kimjunhwan/Desktop/python codingtest/python/soomoongcup-2024-2/problems/고등학생 수뭉이")
@@ -18,15 +18,22 @@ with problem.testcase('1') as sys:
     S = "Hello, World! "
     sys.stdin.write(f'{S}\n')
 
-# 최악의 경우
+# 예제 2
 with problem.testcase('2') as sys:
+    N = 174
+    sys.stdin.write('{N}\n'.format(N=N))
+    S = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation"
+    sys.stdin.write(f'{S}\n')
+
+# 최악의 경우
+with problem.testcase('3') as sys:
     N = MAX_N
     sys.stdin.write('{N}\n'.format(N=N))
     S = ''.join(random.choice(string.ascii_letters + string.digits + string.punctuation) for _ in range(N))
     sys.stdin.write(f'{S}\n')
 
 # 랜덤 테스트 케이스
-for i in range(3, TEST_COUNT + 1):
+for i in range(4, TEST_COUNT + 1):
     with problem.testcase(str(i)) as sys:
         N = random.randint(1, MAX_N)
         sys.stdin.write('{N}\n'.format(N=N))
