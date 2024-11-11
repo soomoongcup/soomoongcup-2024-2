caeda4ade776b5475f1997bac51911884be0bf14
kss418 <108602943+kss418@users.noreply.github.com>
2024-11-11 18:49:52 +0900

    코드 수정

--- problems/직사각형 면적합 구하기_플레/solving_ans_seg.cpp
+++ problems/직사각형 면적합 구하기_플레/solving_ans_seg.cpp
@@ -13,12 +13,12 @@ using ld = long double;
 using ull = unsigned long long;
 using pll = pair<ll, ll>; using tll = tuple<ll, ll, ll>;
 ll n, m, k, t; string s;
-
+ 
 constexpr ll INF = 0x3f3f3f3f3f3f3f3f;
 constexpr ll MAX = 30101;
 constexpr ll MOD = 998244353;
-
-
+ 
+ 
 class _sseg {
 public:
     ll n; vector <ll> arr, seg, c;
@@ -27,7 +27,7 @@ public:
         arr.resize(n + 1); seg.resize(4 * n + 1);
         c.resize(4 * n + 1);
     }
-
+ 
     ll query(ll st, ll en) { return query(st, en, 0, n); }
     ll query(ll st, ll en, ll l, ll r, ll node = 1) {
         if (en < l || st > r) return 0;
@@ -35,23 +35,26 @@ public:
         ll mid = (l + r) >> 1;
         return query(st, en, l, mid, node * 2) + query(st, en, mid + 1, r, node * 2 + 1);
     }
-
+ 
     void add(ll st, ll en, ll val) { add(st, en, val, 0, n); }
     void add(ll st, ll en, ll val, ll l, ll r, ll node = 1) {
         if (r < st || l > en) return;
-
+ 
         if(st <= l && r <= en) c[node] += val;
         else{
             ll mid = (l + r) >> 1;
             add(st, en, val, l, mid, node * 2);
             add(st, en, val, mid + 1, r, node * 2 + 1);
         }
-
-        if(c[node] > 0) seg[node] = r - l + 1;
-        else seg[node] = seg[node * 2] + seg[node * 2 + 1]; 
+ 
+        if(c[node]) seg[node] = r - l + 1;
+        else {
+            if(l == r) seg[node] = 0;
+            else seg[node] = seg[node * 2] + seg[node * 2 + 1]; 
+        }
     }
 };
-
+ 
 class node{
 public:
     ll x, y1, y2, num;
@@ -60,28 +63,33 @@ public:
     }
 };
 vector <node> query;
-
+ 
 int main() {
     fastio;
-
-    cin >> n; _sseg seg(MAX);
-    for(int i = 1;i <= n;i++){
-        ll x1, y1, x2, y2;
-        cin >> x1 >> y1 >> x2 >> y2; y2--;
-        query.push_back({x1, y1, y2, 1});
-        query.push_back({x2, y1, y2, -1});
+    
+    cin >> t;
+    while(t--){
+        cin >> n; _sseg seg(MAX);
+        query.clear();
+        
+        for(int i = 1;i <= n;i++){
+            ll x1, y1, x2, y2;
+            cin >> x1 >> y1 >> x2 >> y2; y2--;
+            query.push_back({x1, y1, y2, 1});
+            query.push_back({x2, y1, y2, -1});
+        }
+        sort(all(query));
+    
+        ll result = 0, la = 0;
+        for(auto&i : query){
+            auto&[x, y1, y2, num] = i;
+            result += seg.query(0, MAX - 1) * (x - la);
+            seg.add(y1, y2, num); la = x;
+        }
+    
+        cout << result << "\n";
     }
-    sort(all(query));
-
-    ll result = 0, la = 0;
-    for(auto&i : query){
-        auto&[x, y1, y2, num] = i;
-        result += seg.query(0, MAX - 1) * (x - la);
-        seg.add(y1, y2, num); la = x;
-    }
-
-    cout << result;
-
-
+ 
+ 
     return 0;
 }
